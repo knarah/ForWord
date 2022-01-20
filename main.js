cae83c649c40d60f8503121461580c52f09868a6
NarahK <kimnarah@gmail.com>
2022-01-20 12:13:29 -0500

    fix details
    - CSS checkbox
    - Get domain name
    - Correct Typo
    - Clear input after submit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Modal
 const loginForm = document.querySelector('.login_form');
 const loginName = document.getElementById('name');
 const loginEmail = document.getElementById('email');
@@ -9,12 +8,19 @@ const modal = document.querySelector('.modal');
 
 // When click login button
 function loginFormOnSubmit(event) {
-    let userName = loginName.value; 
-    let userEmail = loginEmail.value;
-    const domain = userEmail.substring(userEmail.lastIndexOf("@") +1);
+    const userName = loginName.value; 
+    const userEmail = loginEmail.value;
+    const domain = userEmail.substring(userEmail.lastIndexOf('@') +1, userEmail.lastIndexOf('.'));
+    const agreement = document.getElementById('agreement');
+    console.log(agreement);
+  
     event.preventDefault(); 
     openModal(userName, domain);
-
+    // Empty input
+    loginName.value='';
+    loginEmail.value='';
+    //uncheck the checkbox
+    agreement.checked = false;
 }
 
 function openModal(userName, domain){
